a6a5035f1bb067578a70935a922df9d99642a843
steve-kim-96 <soungku@hotmail.com>
2025-06-27 12:10:31 +1200

    fix import

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ChipUsage from './App';
+import { DoggoHome } from './DoggoHome';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <ChipUsage />
+    <DoggoHome />
   </React.StrictMode>
 );
 
