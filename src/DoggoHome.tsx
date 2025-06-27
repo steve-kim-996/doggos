43d743d86b71b761adc77b03ad8495afa9671c4b
steve-kim-96 <soungku@hotmail.com>
2025-06-27 13:35:24 +1200

    extract doggo actions

--- src/DoggoHome.tsx
+++ src/DoggoHome.tsx
@@ -1,14 +1,20 @@
-import { ImageList, ImageListItem } from '@mui/material';
-import { useFetchDoggos } from './doggos/hooks/useDoggos';
+import { Button, ImageList, ImageListItem } from '@mui/material';
+import { useDoDoggoStuff, useFetchDoggos } from './doggos/hooks/useDoggos';
+import { useStyles } from './DogoHome.styles';
 
 export function DoggoHome() {
   const { doggos } = useFetchDoggos()
+  const { classes } = useStyles();
+  const { bark } = useDoDoggoStuff();
 
-  return <ImageList sx={{ width: 700, height: 700 }} cols={5} rowHeight={300}>
+  return <div className={classes.doggoContainer}><ImageList classes={classes} sx={{ width: 3000, height: 1000 }} cols={5} rowHeight={300} gap={300}>
     {doggos.map((doggo, index) => (
-      <ImageListItem key={index}>
-        <img src={doggo} alt='doggo' />
-      </ImageListItem>
+      <Button onClick={() => bark(index)}>
+        <ImageListItem key={index}>
+          <img src={doggo} alt='doggo' />
+        </ImageListItem>
+      </Button>
     ))}
   </ImageList>
+  </div>
 }
--- src/doggos/hooks/useDoggos.ts
+++ src/doggos/hooks/useDoggos.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export const useFetchDoggos = () => {
     const [doggos, setDoggos] = useState<string[]>([])
@@ -7,7 +7,7 @@ export const useFetchDoggos = () => {
         const fetchDogs = async () => {
             try {
                 const doggoResponse = await axios.get('https://dog.ceo/api/breed/hound/images')
-                setDoggos(doggoResponse.data.message.slice(0, 10)) // Limit to first 10 images
+                setDoggos(doggoResponse.data.message.slice(0, 20)) // Limit to first 20 images
             } catch (error) {
                 console.error('Error fetching doggos:', error);
                 throw error;
@@ -20,4 +20,12 @@ export const useFetchDoggos = () => {
     }, [])
 
     return { doggos }
+}
+
+export const useDoDoggoStuff = () => {
+    const bark = useCallback((index: number) => {
+        alert(`Woof! I am dog number ${index + 1}`)
+    }, [])
+
+    return { bark }
 }
