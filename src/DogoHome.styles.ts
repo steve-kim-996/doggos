f88680c29eda01e0230c7d937d4cbb19d012695d
steve-kim-96 <soungku@hotmail.com>
2025-06-27 13:34:58 +1200

    do some basic styling for the doggos

--- src/DogoHome.styles.ts
+++ src/DogoHome.styles.ts
@@ -1,5 +1,9 @@
 import { makeStyles } from 'tss-react/mui'
 
-export const useStyles = makeStyles()((theme) => {
-
-})
+export const useStyles = makeStyles()(() => ({
+    doggoContainer: {
+        display: 'flex',
+        justifyContent: 'center',
+        padding: '20px',
+    }
+}))
